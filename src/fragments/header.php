b80fd17c03846c8d17a7304ca636e52c4314bff9
Vasileios Pasialiokis <whiterocket@outlook.com>
2025-01-26 12:59:54 +0200

    automated deployment

--- src/fragments/header.php
+++ src/fragments/header.php
@@ -3,7 +3,7 @@
 
   <section>
     <div>full-stack developer</div>
-    <a href='mailto:pasivasi@outlook.com'>vas@tsuku.ro</a>
+    <a href='mailto:vas@tsuku.ro'>vas@tsuku.ro</a>
     <a href='tel:+306955018104'>+30 695 501 8104</a>
     <a href='https://github.com/vaaas'>github.com/vaaas</a>
   </section>
